613953a17deccfd8a8a3bf4aa6f2d947a9b64e95
Student <student@LAB308-44.it.kmitl.ac.th>
2017-11-26 18:08:45 +0700

    SignUp Test

---
+++ app/src/androidTest/java/kmitl/mobile/project/bawonsak58070074/tradeevent/AimateIntentTest.java
@@ -0,0 +1,19 @@
+package kmitl.mobile.project.bawonsak58070074.tradeevent;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+import kmitl.mobile.project.bawonsak58070074.tradeevent.controller.LoginActivity;
+
+/**
+ * Created by student on 26/11/2017 AD.
+ */
+
+public class AimateIntentTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+
+
+}
--- app/src/androidTest/java/kmitl/mobile/project/bawonsak58070074/tradeevent/controller/SignUpActivityTest.java
+++ app/src/androidTest/java/kmitl/mobile/project/bawonsak58070074/tradeevent/controller/SignUpActivityTest.java
@@ -1,9 +1,19 @@
 package kmitl.mobile.project.bawonsak58070074.tradeevent.controller;
 
+import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import kmitl.mobile.project.bawonsak58070074.tradeevent.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by Kcomic on 26/11/2560.
@@ -15,7 +25,15 @@ public class SignUpActivityTest {
     public ActivityTestRule<SignUpActivity> mActivityTestRule = new ActivityTestRule<>(SignUpActivity.class);
 
     @Test
-    public void test(){
+    public void SignUpTest(){
+        onView(withId(R.id.new_fullname)).perform(replaceText("Test5 Test"), closeSoftKeyboard());
+        onView(withId(R.id.new_nickname)).perform(replaceText("Test5"), closeSoftKeyboard());
+        onView(withId(R.id.new_username)).perform(replaceText("test5"), closeSoftKeyboard());
+        onView(withId(R.id.new_password)).perform(replaceText("test5"), closeSoftKeyboard());
+        onView(withId(R.id.new_email)).perform(replaceText("test5@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.new_phone)).perform(replaceText("0851245112"), closeSoftKeyboard());
+        onView(withId(R.id.signUpBtn)).perform(click());
+        SystemClock.sleep(3000);
 
     }
 }
